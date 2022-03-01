17b59c2fd970b19c671f4c730435faeb05a48407
VladimirPermgb <100301190+VladimirPermgb@users.noreply.github.com>
2022-03-01 21:39:16 +0300

    Задача 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите первое число");
+// Задача 1
+    
+Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -10,3 +12,28 @@ else
 {
         Console.WriteLine("Второе число больше");
         }
+
+// Задача 2
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a >= b)
+    {
+        max = a;
+    }
+else
+    {
+        max = b;
+    }
+
+if (c >= max)
+{  
+    max = c;
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
