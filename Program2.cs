0a5b1a883f93d868ad179bbab4e875c47a303e55
Vladimir <mail@myexample.com>
2022-03-01 21:17:25 +0300

    initial

---
+++ Program2.cs
@@ -0,0 +1,17 @@
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    {
+        Console.WriteLine("Максимальное число: ");
+        Console.Write(a);
+    }
+else
+{
+        Console.WriteLine("Максимальное число: ");
+        Console.Write(b);
+}
