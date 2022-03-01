2c276902117cb4e9847ac5d987148e0a46bd612d
Vladimir <mail@myexample.com>
2022-03-01 21:14:04 +0300

    Задание 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿//int a=1;
+//int b=2;
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    {
+        Console.WriteLine("Максимальное число: ");
+        Console.Write(a);
+    }
+else
+{
+        Console.WriteLine("Максимальное число: ");
+        Console.Write(b);
+}
